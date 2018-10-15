61e4a60f485a95eed9ec6deeefd0207a7a2fed13
carolinapowers <carolinapoloni@gmail.com>
2018-10-15 10:53:35 -0400

    Add express server to serve production app

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require("express");
+let path = require("path");
+let serveStatic = require("serve-static");
+let history = require("connect-history-api-fallback");
+
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+console.log("remote dir: " + __dirname + "/dist");
+app.use(history());
+
+var port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log("port " + port);
